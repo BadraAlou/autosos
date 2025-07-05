3c074c93adc676ad77f1b229155d53bdbcc47333
Badra <badra10alou10@gmail.com>
2025-07-05 15:30:01 +0000

    backend update

--- sercvie_depannage/settings.py
+++ sercvie_depannage/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -181,5 +185,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STRIPE_SECRET_KEY = "sk_test_..."  # ou mieux : via une variable d'environnement
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
